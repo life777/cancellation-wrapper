1ff32b3ba51c1ccd889f2612d4a1c9d6795b574d
vladimir <dashukevichvova@gmail.com>
2018-11-11 16:51:31 -0500

    Fixed Cancellation token implementation.

--- src/cancellationToken.js
+++ src/cancellationToken.js
@@ -1,22 +1,17 @@
 // It isn't a complete version of token.
 // It will be changed when TC39 decides about cancellation primitive: https://github.com/tc39/proposal-cancellation
 
-export class CancellationTokenSource {
-    constructor (token) {
-        Object.defineProperty(this, "cancellationRequested", {
-            configurable: false,
-            enumerable: false,
-            writable: false,
-            value: false,
-            get: () => token.cancelled
-        });
-    }
-}
-
 export class CancellationToken {
     constructor () {
         this.cancelled = false;
-        this.token = new CancellationTokenSource(this);
+    }
+
+    set cancellationRequested (_) {
+        throw new Error("cancellationRequested can't be changed");
+    }
+
+    get cancellationRequested () {
+        return this.cancelled
     }
 
     cancel () {
